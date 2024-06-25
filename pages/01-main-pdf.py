39c64517ad9347f14f3485701cc22f59b2db4fd9
AnnieHu1006 <90959724+AnnieHu1006@users.noreply.github.com>
2024-06-25 20:30:49 +0800

    Update 01-main-pdf.py
    solve pdf shown problem

--- pages/01-main-pdf.py
+++ pages/01-main-pdf.py
@@ -30,8 +30,8 @@ def corridors():
         fp.write_bytes(uploaded_file.getvalue())
         with open(tmp_file.name, "rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" ' \
-                      f'width="800" height="1000" type="application/pdf">'
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" ' \
+                  f'width="800" height="1000" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
 
 def contains_digit(s):
